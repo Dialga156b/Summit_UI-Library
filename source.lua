d4b6be708b3700571d0fffbe8a3daa6024fca59b
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2025-03-07 18:45:30 -0500

    misc changes

--- source.lua
+++ source.lua
@@ -1979,4 +1979,50 @@ function summitLib:CreateWindow(ConfigArgs)
 	return window
 end
 
+local window = summitLib:CreateWindow({Name = "Summit UI Library Example",AccentColor3 = Color3.new(1, 0.235294, 0.337255)})
+local Tab1 = window:CreateTab({Name = "Labels", Icon = 'rbxassetid://7733765307'})
+local Tab2 = window:CreateTab({Name = "Interactables", Icon = 'rbxassetid://7743875962'})
+local Tab3 = window:CreateTab({Name = "Dropdowns", Icon = 'rbxassetid://7743876142'})
+local Tab4 = window:CreateTab({Name = "Miscecellaneous", Icon = 'rbxassetid://7733920644'})
+Tab1:CreateLabel("Hello! This is a default Label.")
+Tab1:CreateParagraph("This is a default Paragraph Element. the text isn't bolded, slightly smaller, and you could use this for when you want to introduce your users to a selection of items, menus, or variables.")
+
+local SliderValue = 16
+
+Tab2:CreateSlider({Text = "This is a default Slider Element.",Default = 16, Minimum = 0, Maximum = 100, Callback = function(Value)
+	print(Value) -- returns a whole number+
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	SliderValue = Value
+end})
+
+Tab2:CreateButton({Text = "This is a Button! Click me to send a notification!",Callback = function()
+	print("Button Clicked!")
+	window:CreateNotification({Description = "Descriptions can contain variables, like the one on the slider! ["..SliderValue.."]",Title = "Notification",Duration = 5})
+end})
+
+Tab2:CreateToggle({Text = "This is a Toggle!",Default = false,Callback = function(Value)
+	print(Value) --  returns a boolean
+end})
+
+Tab3:CreatePlayerSelector({Text = "This is a player selector!",Callback = function(Player)
+	print(Player.Name)--returns a player
+end})
+
+Tab3:CreateDropdown({Text = "Dropdown",Options = {"Option 1", "Option 2", "Option 3"},Callback = function(Option)
+	print(Option)
+end})
+
+Tab4:CreateColorPicker({Text = "Color Picker",Default = Color3.new(0.866667, 0.203922, 1),Callback = function(Color)
+	print(Color)
+end})
+
+Tab4:CreateBind({Text = "Press Keybind",Hold = false,Default = Enum.KeyCode.F,CallBack = function()
+	print('Keybind Fired!') -- fires when the keybind is pressed.
+end})
+
+Tab4:CreateBind({Text = "Hold Keybind",Hold = true,Default = Enum.KeyCode.E,CallBack = function(isHolding)
+	print('Holding: ',isHolding) -- fires when the keybind is press and unpressed, isHolding being it's current state.
+end})
+
 return summitLib
+
